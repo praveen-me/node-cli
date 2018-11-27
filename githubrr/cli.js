b758ae4c2a8685373ff40c550d13fcb40ca13955
praveen-me <praveenkrsaini0@gmail.com>
2018-11-28 01:18:53 +0530

    Add spinner in case 1

--- githubrr/cli.js
+++ githubrr/cli.js
@@ -3,7 +3,9 @@
 const readline = require('readline');
 const https = require('https');
 const chalk = require('chalk');
-
+const logUpdate = require('log-update');
+const spinner = require('elegant-spinner');
+const frame = spinner();
 
 let username;
 
@@ -14,37 +16,28 @@ const line1 = readline.createInterface({
 
 let data = '';
 
-// line.question('Enter you github username? ', (user) => {
-//   https.get({
-//     host: 'api.github.com',
-//     path: '/users/' + user,
-//     method: 'GET',
-//     headers: {'user-agent': 'node.js'}
-//   }, (res) => {
-//     res.on('data', (d) => {
-//       data = data + d;
-//     })
-//     res.on('end', () => {
-//       data = JSON.parse(data);
-//       console.log(`Name -----------------> \x1b[31m ${data.name} `);
-//       console.log(`Public Repos -----------------> ${data.public_repos}`);
-//       console.log(`Followers -----------------> ${data.followers}`);
-//       })
-
-//       line.close();
-//   })
-// })
-
-
 let userData = '';
+
+let isLoading = false;
 
 line1.question(
   chalk.magenta.italic('What do you want to do?\n 1 - Get Single User Info \n 2 - Github User Battle \n\nEnter you reponse -> '), 
   (value) => {
     if(value === '1') {
       console.log(chalk.rgb(76, 175, 80).bold('\n\nYou select => Get Single User Info'));
-
       line1.question(chalk.rgb(0, 188, 212).bold(`\n\nEnter github user name => `), (user) => {
+
+        isLoading = true;
+        let interval;
+        
+        if(isLoading) {
+            interval =  setInterval(function(){
+              logUpdate(`
+                ${ `Waiting for data for ${user} ${frame()}`}
+              `)
+            }, 50)
+        }
+        
         https.get({
               host: 'api.github.com',
               path: '/users/' + user,
@@ -59,24 +52,31 @@ line1.question(
               })
               res.on('end', () => {
                 userData = JSON.parse(userData);
+                isLoading = false;
+                if(!isLoading) {
+                  clearInterval(interval);
+                  // Showing User Data
+                  console.log(`\n${chalk.rgb(4, 114, 202).bold(`${userData.name}`)}(${chalk.rgb(4, 114, 202).bold(`${userData.login}`)})`);
+                  
+                  console.log(`${chalk.rgb(255, 193, 7).bold(`${userData.bio}`)}`);
+                  
+                  console.log(`\n${chalk.rgb(255, 152, 0).bold('Repositories')}\t\t\t::\t${chalk.magenta.bold(`${userData.public_repos}`)}`);
+                  
+                  console.log(`\n${chalk.rgb(255, 152, 0).bold('Followers')}\t\t\t::\t${chalk.magenta.bold(`${userData.followers}`)}`);
 
-                // Showing User Data
-                console.log(`\n${chalk.rgb(4, 114, 202).bold(`${userData.name}`)}(${chalk.rgb(4, 114, 202).bold(`${userData.login}`)})`);
-                console.log(`${chalk.rgb(255, 193, 7).bold(`${userData.bio}`)}`);
-                console.log(`\n${chalk.rgb(255, 152, 0).bold('Repositories')}\t\t\t::\t${chalk.magenta.bold(`${userData.public_repos}`)}`);
-                console.log(`\n${chalk.rgb(255, 152, 0).bold('Followers')}\t\t\t::\t${chalk.magenta.bold(`${userData.followers}`)}`);
-                console.log(`\n${chalk.rgb(255, 152, 0).bold('Following')}\t\t\t::\t${chalk.magenta.bold(`${userData.following}`)}`);
+                  console.log(`\n${chalk.rgb(255, 152, 0).bold('Following')}\t\t\t::\t${chalk.magenta.bold(`${userData.following}`)}`);
 
-                })
+                  line1.close();
+                }
+              })
             })
           })
     } else if(value === '2') {
-      console.log(chalk.rgb(76, 175, 80).bold('You select => Github User Battle'))
+      console.log(chalk.rgb(76, 175, 80).bold('You select => Github User Battle'));
+      console.log('This option is in development');
     } else {
       console.log(chalk.red.bold("Enter correct choice"))
     }
-
-    // line1.close();
   })
 
 
